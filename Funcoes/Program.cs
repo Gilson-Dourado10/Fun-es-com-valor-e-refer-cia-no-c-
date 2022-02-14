443559d3c25d65ef5ebb2b66c927e39bb736c19c
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com.br>
2022-02-14 06:06:05 -0300

    funções com valor e refência no c#

---
+++ Funcoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            dobrar(ref num);
+            Console.WriteLine(num);
+            imprime_linha();
+        }
+
+        static void imprime_linha()
+        {
+            Console.WriteLine("\n*****-------------*****---------------******\n");
+        }
+
+        static void dobrar(ref int valor)
+        {
+            valor *= 2;
+        }
+    }
+}
